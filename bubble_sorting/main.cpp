e137b1e7b7a1cc8415598c5eee95646651b4dd65
skx1322 <kaixing636@gmail.com>
2025-12-22 17:26:27 +0800

    bubble sorting testing

---
+++ bubble_sorting/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+const int array_size = 10;
+
+//Swap Function, Argument integer one and integer two
+void swap(int& num1, int& num2)
+{
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+
+void bubble_sort(int *numbers)
+{
+    bool is_swapped;
+    for (int i = 0; i < array_size; i++)
+    {
+        is_swapped = false;
+        for (int j = 0; j < array_size - i - 1; j++)
+        {
+            if (numbers[j] > numbers[j + 1])
+            {
+                swap(numbers[j], numbers[j + 1]);
+                is_swapped = true;
+            }
+        }
+        if (!is_swapped)
+        {
+            break;
+        }
+        
+    }
+}
+
+void display_array(int *numbers)
+{
+    for (int i = 0; i < array_size; i++)
+    {
+        cout << numbers[i] << ", ";
+    };
+    cout << endl;
+}
+
+int main()
+{
+    int sample_number[array_size] = {6, 42, 65, 34, 12, 8, 41, 7, 42, 24};
+
+    bubble_sort(sample_number);
+
+    display_array(sample_number);
+    return 0;
+}
